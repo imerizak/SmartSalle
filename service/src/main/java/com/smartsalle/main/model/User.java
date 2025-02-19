4ee0b9305e2206390b653e7a798d44e90c412298
Ibrahim Merizak <ibrahim.merizak@yahoo.com>
2025-02-20 00:22:01 +0100

    [feat] Add user model

---
+++ service/src/main/java/com/smartsalle/main/model/User.java
@@ -0,0 +1,45 @@
+package com.smartsalle.main.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "password_hash", nullable = false)
+    private String passwordHash;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole role;  // Enum: CLIENT, MANAGER, TRAINER
+
+    private String firstName;
+    private String lastName;
+    @Column(name = "organization", nullable = false, unique = true)
+    private String organization;
+    @Column(name = "phone", nullable = false, unique = true)
+    private String phone;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Membership> memberships = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Booking> bookings = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<WorkoutPlan> workoutPlans = new ArrayList<>();
+
+    @OneToMany(mappedBy = "trainer", cascade = CascadeType.ALL)
+    private List<Session> sessions = new ArrayList<>();
+}
---
+++ service/src/main/java/com/smartsalle/main/model/UserRole.java
@@ -0,0 +1,5 @@
+package com.smartsalle.main.model;
+
+public enum UserRole {
+    CLIENT, MANAGER, TRAINER
+}
